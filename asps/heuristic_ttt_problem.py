34960ef95a6946078ee8de52da75bac77efd01b7
hongzheng24 <hong_zheng@brown.edu>
2025-09-24 18:38:47 -0400

    feat(TTT heuristic): TTTProblem heuristic

--- asps/heuristic_ttt_problem.py
+++ asps/heuristic_ttt_problem.py
@@ -14,9 +14,27 @@ class HeuristicTTTProblem(TTTProblem, HeuristicAdversarialSearchProblem):
         function works with boards of any size; if it only works for 3x3 boards, you won't be
         able to properly test ab-cutoff for larger board sizes!
         """
-        return
         board = state.board
         n = len(board)
+        score = 0
 
-        def score(line):
-            return
+        def find_score(line):
+            if all(cell in (None, 'X') for cell in line):
+                count = line.count('X')
+                return count**2
+            elif all(cell in (None, 'O') for cell in line):
+                count = line.count('O')
+                return -count**2
+            return 0
+        
+        for row in board:
+            score += find_score(row)
+        
+        for j in range(n):
+            col = [board[i][j] for i in range(n)]
+            score += find_score(col)
+
+        score += find_score([board[i][i] for i in range(n)])
+        score += find_score([board[i][n-1-i] for i in range(n)])
+
+        return score
